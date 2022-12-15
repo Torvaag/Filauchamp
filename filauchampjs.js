baa2ac146b86c33fb5a87a6597a1dc084afc56c9
Torvaag <thomas.foinont@stagiairesmns.fr>
2022-12-15 09:21:51 +0100

    Initial commit

---
+++ filauchampjs.js
@@ -0,0 +1,8 @@
+function show() {
+ 
+    document.getElementById('z0')
+            .style.opacity = 0.9;
+                        
+    document.getElementById('z1')
+            .style.opacity = 0;
+        }
